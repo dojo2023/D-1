9fda5a5c5f6e918e161547a2f0216c1dd8881931
dojo2023-kato <130331046+dojo2023-kato@users.noreply.github.com>
2023-06-14 16:58:30 +0900

    Update graph.js

--- src/WebContent/js/graph.js
+++ src/WebContent/js/graph.js
@@ -11,23 +11,92 @@ document.addEventListener("DOMContentLoaded", function() {
 		        backgroundColor: "rgba(0,0,0,0)"
 			}],
 		},
-		option: {
-		    title: {
-		        display: true,
-		        text: '気温（8月1日~8月7日）'
+		options: {
+		    plugins: {
+		      // グラフタイトル
+		      	title: {
+		        	display: true,
+		        	text: '摂取カロリー',
+		        	color: 'black',
+		        	padding: {top: 5, bottom: 5},
+		        	font: {
+		        	  	family: '"Arial", "Times New Roman"',
+		        		size: 12,
+		        	},
+		      	},
+		      // 凡例
+		      	legend: {
+		        	position: 'bottom',
+		        	align: 'end',
+		        // 凡例ラベル
+		        	labels: {
+		          		boxWidth: 20,
+		          		boxHeight: 8,
+		        	},
+		        // 凡例タイトル
+		        	title: {
+		          		display: true,
+		          		text: '日付',
+		          		padding: {top: 20},
+		        	},
+		      	},
+		      // ツールチップ
+		      	tooltip: {
+		        	backgroundColor: '#933',
+		      	},
 		    },
 		    scales: {
-		        yAxes: [{
+		      	y: {
+		        // 最小値・最大値
+		        	min: 0,
+		        	max: 60,
+		        // 軸タイトル
+		        	title: {
+		          		display: true,
+		          		text: '摂取カロリー',
+		          		color: 'black',
+		        	},
+		        // 目盛ラベル
 		        	ticks: {
-		            	suggestedMax: 40,
-		            	suggestedMin: 0,
-		            	stepSize: 10,
-		            	callback: function(value){
-		            		return  value +  '度'
-		            	}
-		          	}
-		       	}]
-		    }
-		}
+		          		color: 'blue',
+		          		stepSize: 20,
+		          		showLabelBackdrop: true,
+		          		backdropColor: '#ddf',
+		          		backdropPadding: { x: 4, y: 2 },
+		          		major: {
+		          		  	enabled: true,
+		          		},
+		          		align: 'end',
+		          		crossAlign: 'center',
+		          		sampleSize: 4,
+		        	},
+		        	grid: {
+		          // 軸線
+			          	borderColor: 'orange',
+			          	borderWidth: 2,
+			          	drawBorder: true,
+			          // 目盛線＆グリッド線
+			          	color: '#080',
+			          	display: true,
+			          // グリッド線
+			          	borderDash: [3, 3],
+			          	borderDashOffset: 0,
+			          	// 目盛線
+			          	drawTicks: true,
+			          	tickColor: 'blue',
+			          	tickLength: 10,
+			          	tickWidth: 2,
+			          	tickBorderDash: [2, 2],
+			          	tickBorderDashOffset: 0,
+		        	},
+		      	},
+		      	x: {
+		        	grid: {
+		          		borderColor: 'orange',
+		          		borderWidth: 2,
+		        	},
+		      	},
+		    },
+		},
 	})
 })
