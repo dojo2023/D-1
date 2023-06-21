7287b11dd7c94b74cab820e19a8f14b4daad30a6
dojo2023-yamamoto-j <130334426+dojo2023-yamamoto-j@users.noreply.github.com>
2023-06-21 09:47:42 +0900

    update

--- src/src/servlet/Log_Servlet.java
+++ src/src/servlet/Log_Servlet.java
@@ -33,48 +33,41 @@ public class Log_Servlet extends HttpServlet {
 		HttpSession session = request.getSession();
 		Loggedin user_addr = (Loggedin)session.getAttribute("user_addr");
 
-		//カテゴリーを取得
-		FoodDao Fdao = new FoodDao();
-		List<Food> f_category = Fdao.cat_select();
+		if (user_addr == null) {
 
-		//情報をセットしておく
-		request.setAttribute("f_category", f_category);
+			//カテゴリーを取得
+			FoodDao Fdao = new FoodDao();
+			List<Food> f_category = Fdao.cat_select();
 
-		//カレンダーから押された時の処理
-		request.setCharacterEncoding("UTF-8");
-		if(request.getParameter("GETmonth") != null) {
-			String year = request.getParameter("GETyear");
-			String month = String.format("%02d",Integer.parseInt(request.getParameter("GETmonth")));
-			String date = String.format("%02d",Integer.parseInt(request.getParameter("GETdate")));
-			String day = year + "-" + month + "-" + date;
-			System.out.println(day);
-			request.setAttribute("day", day);
-		}else {
-			//日付をyyyyMMddの形で出力する
-			SimpleDateFormat dtf = new SimpleDateFormat("yyyy-MM-dd");
-	        Calendar calendar = Calendar.getInstance();
+			//情報をセットしておく
+			request.setAttribute("f_category", f_category);
 
-	        Date dateObj = calendar.getTime();
-	        String formattedDate = dtf.format(dateObj);
-	        request.setAttribute("day", formattedDate);
-		}
-        //画面へフォワード
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/log.jsp");
-		dispatcher.forward(request, response);
+			//カレンダーから押された時の処理
+			request.setCharacterEncoding("UTF-8");
+			if(request.getParameter("GETmonth") != null) {
+				String year = request.getParameter("GETyear");
+				String month = String.format("%02d",Integer.parseInt(request.getParameter("GETmonth")));
+				String date = String.format("%02d",Integer.parseInt(request.getParameter("GETdate")));
+				String day = year + "-" + month + "-" + date;
+				System.out.println(day);
+				request.setAttribute("day", day);
+			}else {
+				//日付をyyyyMMddの形で出力する
+				SimpleDateFormat dtf = new SimpleDateFormat("yyyy-MM-dd");
+		        Calendar calendar = Calendar.getInstance();
 
-		/*
-		//user_addrがあるのか判定
-		//if (user_addr == null) {
-
-			//user_numを取得
-			UserDao Udao = new UserDao();
-			int id = Udao.getNum(user_addr);
+		        Date dateObj = calendar.getTime();
+		        String formattedDate = dtf.format(dateObj);
+		        request.setAttribute("day", formattedDate);
+			}
+	        //画面へフォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/log.jsp");
+			dispatcher.forward(request, response);
 
 		}else {
 			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/top.jsp");
 			dispatcher.forward(request, response);
 		}
-		*/
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
