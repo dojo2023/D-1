37f5ebf6be45637a50d92498a8f8885af88a28ad
dojo2023-inose <130331134+dojo2023-inose@users.noreply.github.com>
2023-06-27 13:50:44 +0900

    Update password.js

--- src/WebContent/js/password.js
+++ src/WebContent/js/password.js
@@ -1,3 +1,13 @@
-/**
- * 
- */
+// パスワードの確認
+function validateForm() {
+	var password = document.getElementById("password").value;
+	var confirmpassword = document.getElementById("confirmpassword").value;
+
+	if (password !== confirmpassword) {
+		document.getElementById("errorText").innerText = "パスワードが一致しません";
+		return false; // フォームの送信をキャンセル
+	}
+
+	// パスワードが一致した場合はフォームを送信
+	return true;
+}
